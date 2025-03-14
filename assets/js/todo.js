18d177a75f8a45a4252de6c68b74a7f9caaee868
YouTuber-repopo <mitchycraft.java@gmail.com>
2025-03-15 01:56:24 +0900

    :sparkles: feat: Todoリストに期日機能追加

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,14 +1,23 @@
-// ページ読み込み時ローカルストレージからタスク取得
 window.onload = function () {
   const savedTasks = JSON.parse(localStorage.getItem("tasks")) || []; // ローカルストレージ取得
   savedTasks.forEach((t) => drawTask(t)); // それぞれを描画
+
+  resetInput();
 };
 
-// タスク描画処理
-function drawTask(task) {
-  const $taskList = document.getElementById("list"); // タスクリスト取得
 
-  // タスクdiv
+// 新規作成検知
+document.getElementById("add").addEventListener("click", createTask); // 追加ボタンが押された場合
+document.getElementById("add-text").addEventListener("keypress", (e) => {
+  if (e.key === "Enter") createTask(); // Enterが押下された場合
+});
+
+
+// タスク描画
+function drawTask(task) {
+  const $taskList = document.getElementById("list");
+
+  // タスクボックスdiv
   const $taskBox = document.createElement("div");
   task.completed
     ? ($taskBox.className = "task-box completed")
@@ -24,7 +33,7 @@ function drawTask(task) {
   $taskCompletedCheck.setAttribute("type", "checkbox");
   $taskCompletedCheck.checked = task.completed;
 
-  // 完了・未完了切り替え処理
+  // 完了・未完了トグル
   const toggleCheckbox = () => {
     task = toggleCompleteTask(task);
     task && task.completed
@@ -37,65 +46,74 @@ function drawTask(task) {
   $taskCompletedCheck.addEventListener("click", toggleCheckbox); // 文字がクリックされた場合
   $taskText.addEventListener("click", toggleCheckbox); // チェックボックスがクリックされた場合
 
+  // 日付
+  const $duedate = document.createElement('p');
+  $duedate.textContent = '期日: ' + task.duedate;
+
   // 削除ボタン
   const $deleteButton = document.createElement("button");
   $deleteButton.className = "delete button border";
   $deleteButton.textContent = "削除";
-  $deleteButton.addEventListener("click", () => {
-    deleteTask(task);
-    $taskList.removeChild($taskBox);
-  }); // 削除
+  $deleteButton.addEventListener("click", () => { 
+    deleteTaskFromLocalStorage(task); // ローカルストレージ削除
+    $taskList.removeChild($taskBox); // 要素削除
+  });
 
   // 描画
   $taskText.appendChild($taskCompletedCheck);
   $taskBox.appendChild($taskText);
+  $taskBox.appendChild($duedate);
   $taskBox.appendChild($deleteButton);
   $taskList.appendChild($taskBox);
 }
 
-// 新規作成検知
-document.getElementById("add").addEventListener("click", makeTask); // 追加ボタンが押された場合
-document.getElementById("add-text").addEventListener("keypress", (e) => {
-  if (e.key === "Enter") makeTask();
-}); // Enterが押下された場合
 
-// タスク新規作成処理
-function makeTask() {
+// タスク新規作成
+function createTask() {
   const taskText = document.getElementById("add-text").value.trim(); // テキスト取得
+  const duedate = document.getElementById('due').value;
 
   // タスク描画&ローカルストレージに保存
   if (taskText) {
-    const task = { text: taskText, completed: false };
+    const task = { text: taskText, duedate: duedate, completed: false };
 
     drawTask(task); // 描画
 
-    // ローカルストレージに反映
+    // ローカルストレージに保存
     const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    savedTasks.push({ text: taskText, completed: false });
+    savedTasks.push(task);
     localStorage.setItem("tasks", JSON.stringify(savedTasks));
 
-    document.getElementById("add-text").value = ""; // 入力欄リセット
+    resetInput();
   }
 }
 
-// タスク削除処理
-function deleteTask(task) {
+
+// ローカルストレージからタスク削除
+function deleteTaskFromLocalStorage(task) {
   const savedTasks = JSON.parse(localStorage.getItem("tasks")) || []; // ローカルストレージ取得
   const updatedTasks = savedTasks.filter((t) => t.text != task.text); // 指定のタスクを除いたリスト
 
-  localStorage.setItem("tasks", JSON.stringify(updatedTasks)); // 反映
+  localStorage.setItem("tasks", JSON.stringify(updatedTasks)); // ローカルストレージ反映
 }
 
-// タスクローカルストレージ更新処理
+
+// 完了・未完了トグル
 function toggleCompleteTask(task) {
-  // ローカルストレージ取得
-  const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  const taskId = savedTasks.findIndex((t) => t.text == task.text);
+  const savedTasks = JSON.parse(localStorage.getItem("tasks")) || []; // ローカルストレージ取得
+  const taskIndex = savedTasks.findIndex((t) => t.text == task.text);
 
-  if (taskId == -1) return; // ローカルストレージにタスクが見つからなければreturn
+  if (taskIndex == -1) return; // タスクが見つからなければreturn
 
-  savedTasks[taskId].completed = !savedTasks[taskId].completed; // 切り替え
-  localStorage.setItem("tasks", JSON.stringify(savedTasks)); // 反映
+  savedTasks[taskIndex].completed = !savedTasks[taskIndex].completed; // 完了・未完了トグル
+  localStorage.setItem("tasks", JSON.stringify(savedTasks)); // 保存
 
-  return savedTasks[taskId]; // 切り替え済みタスク返却
+  return savedTasks[taskIndex]; // トグル済みタスクreturn
+}
+
+
+// 入力フォームリセット
+function resetInput() {
+  document.getElementById("add-text").value = ""; // タスク入力欄リセット
+  document.getElementById('due').value = new Date().toLocaleDateString('sv-SE'); // yyyy-mm-dd 方式で期日を設定
 }
